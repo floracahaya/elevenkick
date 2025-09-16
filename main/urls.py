b58c088ccd2f52dcda7f9027faa64e542513b651
floracahaya <floracahayaputri@gmail.com>
2025-09-16 21:49:04 +0700

    Tugas 3 (Updated)

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
-from .views import show_main, products_json, product_json_by_id, products_xml, product_xml_by_id
-from .views import product_list, product_add, product_detail
+from .views import show_main, product_list, product_add, product_detail
+from .views import products_json, product_json_by_id, products_xml, product_xml_by_id
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', product_list, name='product_list'),
-    path('main/', show_main, name='show_main'),
-    path('add/', product_add, name='product_add'),
-    path('<int:pk>/', product_detail, name='product_detail'),
+    path('', show_main, name='show_main'),  # ini yang harusnya jadi halaman utama
+    path('products/', product_list, name='product_list'),
+    path('products/add/', product_add, name='product_add'),
+    path('products/<int:pk>/', product_detail, name='product_detail'),
+
+    # API endpoints
     path('json/', products_json, name='products_json'),
     path('json/<int:pk>/', product_json_by_id, name='product_json_by_id'),
     path('xml/', products_xml, name='products_xml'),
