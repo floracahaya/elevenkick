e59d733aab62cc865483aa0a752e655048044be6
floracahaya <floracahayaputri@gmail.com>
2025-09-16 22:48:31 +0700

    Tugas 3

--- elevenkick/settings.py
+++ elevenkick/settings.py
@@ -34,6 +34,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'flora-cahaya-elevenkick.pbp.cs.ui.ac.id']
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://flora-cahaya-elevenkick.pbp.cs.ui.ac.id"
+]
+
 # Application definition
 
 INSTALLED_APPS = [
